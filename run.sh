b130dcfb84b181923cdfdf6507481fe42944d2f3
tunapro <tuna.gul@nfrproducts.com>
2025-04-01 03:32:41 +0300

    aaaa

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ docker-compose down
 docker-compose build
 docker-compose up -d
 
-echo "App should be running at: http://quick-schedule.tunapro.xyz"
+echo "App should be running at: http://quick-schedule.tunapro.xyz:8080"
 echo "To stop it, run: docker-compose down"
 echo "To check app logs, run: docker-compose logs -f app"
 echo "To check Nginx logs, run: docker-compose logs -f nginx" 
